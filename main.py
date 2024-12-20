4999a8f88cbe51e76000131aad56c9d6ffc952d8
Foo42 <2010117+Foo42@users.noreply.github.com>
2024-12-20 12:34:45 +0000

    Very basic RAG chat

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import sys
 from openai import OpenAI
+from openai.types.chat import (
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionMessageParam,
+    ChatCompletionSystemMessageParam,
+    ChatCompletionUserMessageParam,
+)
 from pathlib import Path
+from typing import Optional
 import numpy as np
 import base64
 
@@ -67,6 +74,62 @@ class EmbeddingStore:
         return float(cos_sim)
 
 
+class Chat:
+    def __init__(
+        self,
+        model_client: OpenAI,
+        model_name: str,
+        system_prompt: Optional[str] = None,
+        embedding_store: Optional[EmbeddingStore] = None,
+    ):
+        self._client = model_client
+        self.model_name = model_name
+        self.conversation: list[ChatCompletionMessageParam] = []
+        self._db = embedding_store
+        if system_prompt is not None:
+            self.conversation.append(self._make_system_message(system_prompt))
+
+    def _make_system_message(self, content: str) -> ChatCompletionMessageParam:
+        return ChatCompletionSystemMessageParam(role="system", content=content)
+
+    def _make_user_message(self, content: str) -> ChatCompletionMessageParam:
+        return ChatCompletionUserMessageParam(role="user", content=content)
+
+    def _make_assistant_message(self, content: str) -> ChatCompletionMessageParam:
+        return ChatCompletionAssistantMessageParam(role="assistant", content=content)
+
+    def _load_as_rag_source(self, file: Path) -> ChatCompletionMessageParam:
+        with open(file, "r") as f:
+            content = f.read()
+        message = f"In answering questions, the following content from a file may be useful:\n {content}"
+        return self._make_user_message(message)
+
+    def add_message(self, message: str) -> str:
+        if message.endswith("?") and self._db is not None:
+            useful_files = [
+                Path(name)
+                for name, similarity in self._db.get_closest_n(message, 3)
+                if similarity >= 0.5
+            ]
+            rag_messages = [self._load_as_rag_source(file) for file in useful_files]
+            print(f"Adding context: {[file.name for file in useful_files]}")
+            for rag_message in rag_messages:
+                self.conversation.append(rag_message)
+
+        self.conversation.append(self._make_user_message(message))
+
+        completion = self._client.chat.completions.create(
+            model=self.model_name,
+            messages=self.conversation,
+            temperature=0.8,
+        )
+
+        new_message = completion.choices[0].message
+        assert new_message.content is not None
+        self.conversation.append(self._make_assistant_message(new_message.content))
+        return new_message.content
+
+
 def find_files(root: Path, pattern="*.md") -> list[str]:
     return [
         str(file_path.resolve())
@@ -90,6 +153,20 @@ def get_n_closest_notes(
     return [Path(file) for file, distance in matches if distance >= threshold]
 
 
+def initiate_chat(chat: Chat):
+    def input_generator():
+        """A generator that yields user input lines until 'stop' is entered."""
+        while True:
+            user_input = input("You say: ")
+            if user_input.lower() == "stop":
+                break
+            yield user_input
+
+    for user_input in input_generator():
+        reply = chat.add_message(user_input)
+        print(f"Reply: {reply}")
+
+
 def main():
     client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
     db = EmbeddingStore(client, Path("/tmp/embeddings"))
@@ -101,6 +178,15 @@ def main():
             paths = [str(path) for path in get_n_closest_notes(db, text, int(n))]
             for path in paths:
                 print(path)
+        case ["chat"]:
+            initiate_chat(
+                Chat(
+                    client,
+                    "llama-3.2-3b-instruct",
+                    "You are a helpful professional who treats content from files as a useful reference when answering questions. When replying try not to include too much extraneous content",
+                    db,
+                )
+            )
         case _:
             print("unrecognised command")
 
